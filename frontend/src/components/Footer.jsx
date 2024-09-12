bda37270d48c8a292951941431d2881ea7008ba4
ayhan219 <ayhan-games@hotmail.com>
2024-09-12 22:47:36 +0300

    footer changed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div>
           <img src={assets.logo} className="mb-5 w-32" alt="" />
           <p className="w-full md:w-2/3 text-gray-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+            Loremm ipsum dolor sit amet consectetur adipisicing elit. Minima,
             sit?
           </p>
         </div>
